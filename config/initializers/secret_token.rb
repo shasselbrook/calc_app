345dee5a081df16d5a4f21f0f033e2247c6553aa
Sally Hasselbrook <shasselbrook@earthlink.net>
2013-07-25 08:40:26 -0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalcApp::Application.config.secret_token = '34e7331af7dac45abf1bf4570063d62eb11a3b1eeae5bef9004c8c7226db204680ac412e7144e5dd6c0bda760892cc0c1a4bde27c97119358807037629d85243'
